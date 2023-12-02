00d733db5457d99029bb3a42214ac360d7efae86
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-12-02 09:26:38 +0500

    Add project files.

---
+++ Tyuiu.KultyshevaEA.Sprint5.TaskReview.V2.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KultyshevaEA.Sprint5.TaskReview.V2.Lib;
+
+using System.IO;
+
+namespace Tyuiu.KultyshevaEA.Sprint5.TaskReview.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask7V2.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KultyshevaEA.Sprint5.TaskReview.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KultyshevaEA.Sprint5.TaskReview.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KultyshevaEA.Sprint5.TaskReview.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dbd6a865-31bb-4362-974e-cd3461bd40eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
